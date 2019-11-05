caad286818cf78aeafc680be1bf6329827058e7e
LuizGX <luizxvr@hotmail.com>
2019-11-05 14:13:25 -0300

    estatisticas sexo layout

--- application/views/estatisticas.php
+++ application/views/estatisticas.php
@@ -1,1 +1,52 @@
-pagina estatisticas
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/estatisticas.css'); ?>">
+	<!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Pessoa');
+        data.addColumn('number', 'Sexo');
+        data.addRows([
+          ['Homens', 3],
+          ['Mulheres', 10]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Homens x Mulheres',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>	
+</head>
+<body>
+
+<body>
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div"></div>
+  </body>
+
+</body>
+</html>
