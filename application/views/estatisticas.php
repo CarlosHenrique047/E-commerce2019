1f2d5f2ed93b2754d27decc040d58fcdada492b8
LuizGX <luizxvr@hotmail.com>
2019-11-08 11:39:32 -0300

    estatisticas preparadas para receber o back

--- application/views/estatisticas.php
+++ application/views/estatisticas.php
@@ -9,19 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
       google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
       google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
       function drawChart() {
-
-        // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Pessoa');
         data.addColumn('number', 'Sexo');
@@ -29,23 +19,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           ['Homens', 3],
           ['Mulheres', 10]
         ]);
-
-        // Set chart options
         var options = {'title':'Homens x Mulheres',
                        'width':400,
                        'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
-    </script>	
+    </script>
+
+<!-- Fim -->
+
+<script type="text/javascript">
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Pessoa');
+  data.addColumn('number', 'Item');
+  data.addRows([
+    ['Livro', 1],
+    ['Jogos', 1],
+    ['Móveis', 1],
+    ['Eletrodomésticos', 1],
+    ['Brinquedos', 1],
+    ['Informática', 1]
+  ]);
+  var options = {'title':'Itens Preferidos',
+                 'width':540,
+                 'height':300};
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div2'));
+  chart.draw(data, options);
+} 
+</script>	
 </head>
 <body>
 
 <body>
     <!--Div that will hold the pie chart-->
     <div id="chart_div"></div>
+
+    <div class="container-media">
+      <h1>Média de idades</h1>
+      <p class="media-idades"> <?php echo "25"; ?> </p>
+    </div>
+
+    <div id="chart_div2"></div>
   </body>
 
 </body>
